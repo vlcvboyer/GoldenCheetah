bf756ef40cbbae982d84c6cf2958f801302dad4b
Vianney BOYER <vlcvboyer@gmail.com>
2023-01-31 21:12:54 +0100

    WIP - write/read AltPower from CSV

--- src/Train/TrainSidebar.h
+++ src/Train/TrainSidebar.h
@@ -250,7 +250,7 @@ class TrainSidebar : public GcWindow
         // updated with a RealtimeData object either from
         // update() - from a push device (quarqd ANT+)
         // Device->getRealtimeData() - from a pull device (Computrainer)
-        double displayPower, displayHeartRate, displayCadence, displaySpeed;
+        double displayPower, displayAltPower, displayHeartRate, displayCadence, displaySpeed;
         double displayLRBalance, displayLTE, displayRTE, displayLPS, displayRPS;
         double displaySMO2, displayTHB, displayO2HB, displayHHB;
         double displayDistance, displayWorkoutDistance;
@@ -264,7 +264,6 @@ class TrainSidebar : public GcWindow
         double textPositionEmitted;
         bool useSimulatedSpeed;
         int displayPosition; // cyclist position (0: seated ; 1: standing ; 2: aero)
-        double displayAltPower;
 
         void maintainLapDistanceState();
 
